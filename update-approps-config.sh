7b799075b04a403426b35d403b151c4809a88a4b
gloufac <lpez.gloria2@gmail.com>
2025-03-24 20:18:05 -0500

    Updates approps config

--- update-approps-config.sh
+++ update-approps-config.sh
@@ -53,23 +53,23 @@ fi
 
 echo "2. Hacer peticion para obtener el token de invitado"
 JSON_DATA='{"username": "'"$LOC_GUEST_USERNAME"'", "password": "'"$LOC_GUEST_PASSWORD"'"}'
-echo "JSON_DATA: $JSON_DATA" | jq .
-
-LOC_VITE_TOKEN=$(curl --silent --show-error --write-out "HTTPSTATUS:%{http_code}" --location 'http://localhost:8080/api/auth/login' \
+RESPONSE=$(curl --silent --show-error --write-out "HTTPSTATUS:%{http_code}" --location 'http://localhost:8080/api/auth/login' \
   --header 'Content-Type: application/json' \
   --data-raw "$JSON_DATA")
-#}' | jq -r '.data.access_token')
 
 # Extraer el código de estado y la respuesta
-BODY=$(echo "$LOC_VITE_TOKEN" | sed -E 's/HTTPSTATUS\:[0-9]{3}$//')
-STATUS=$(echo "$LOC_VITE_TOKEN" | grep -oE '[0-9]{3}$')
+###BODY=$(echo "$LOC_VITE_TOKEN" | sed -E 's/HTTPSTATUS\:[0-9]{3}$//')
+##echo "Response Body: $BODY"
+STATUS=$(echo "$RESPONSE" | grep -oE '[0-9]{3}$')
 echo "HTTP Status: $STATUS"
-echo "Response Body: $BODY"
+
 # Validar si la solicitud fue exitosa
 if [ "$STATUS" -ne 200 ]; then
   echo "Error: La solicitud falló con código $STATUS"
   exit 1
 fi
+
+$LOC_VITE_TOKEN=$(echo "$RESPONSE" | jq -r '.data.access_token')
 
 # Verificar si el TOKEN se obtuvo correctamente
 if [[ -z "$LOC_VITE_TOKEN" || "$LOC_VITE_TOKEN" == "null" ]]; then
